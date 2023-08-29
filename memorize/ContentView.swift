a35a98793a82a5b928ac94b66a11ca6abdf0e9d4
Alan Zhang <zfchang_ai@icloud.com>
2023-08-29 09:00:37 +0800

    build the initial cards view

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️","🧙", "🙀", "👹", "😱", "☠️","🍭"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            CardsView()
             Text("Hello, Alan!")
         }
         .padding()
+        .foregroundColor(.orange)
+    }
+}
+
+struct CardsView: View {
+    let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️","🧙", "🙀", "👹", "😱", "☠️","🍭"]
+    let minimalCards = 1
+    let initialCards = 4
+    let gridColumns = [GridItem(), GridItem(), GridItem(), GridItem()]
+    var body: some View{
+        LazyVGrid (columns: gridColumns){
+            ForEach (0..<initialCards, id: \.self) { index in
+                CardView(symbol: emojis[index])
+            }
+        }
+    }
+}
+struct CardView: View {
+    let symbol: String
+    @State var isFaceUp = false
+    @State var opacity = 0.0
+    var body: some View {
+        ZStack{
+            let base = RoundedRectangle(cornerSize: CGSize(width: 12, height: 12))
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+            }else{
+                base.fill()
+            }
+            Text(symbol)
+                .imageScale(Image.Scale.large)
+                .opacity(opacity)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+            opacity = (isFaceUp ? 1.0 : 0.0)
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+
     }
 }
 
